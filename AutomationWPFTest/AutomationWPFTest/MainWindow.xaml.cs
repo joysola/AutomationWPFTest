76c04163a7865ec0d88df8365d2290b72bc6f8bf
joysola <315718801@qq.com>
2022-11-18 14:16:44 +0800

    1. 上传第一版自动化测试

---
+++ AutomationWPFTest/AutomationWPFTest/MainWindow.xaml.cs
@@ -0,0 +1,100 @@
+﻿using AutomationWPFTest.Hotkey;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AutomationWPFTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            RegisterHotKeys();
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            base.OnClosing(e);
+            // Need to shutdown the hook. idk what happens if
+            // you dont, but it might cause a memory leak.
+            HotkeysManager.ShutdownSystemHook();
+        }
+        private void ChooseDirBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "EXE(*.exe)|*.exe",
+                Multiselect = false,
+                CheckPathExists = true,
+                RestoreDirectory = true,
+            };
+            if (ofd.ShowDialog() == true)
+            {
+                DirTxt.Text = ofd.FileName;
+            }
+        }
+
+        private void TestBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(DirTxt.Text))
+            {
+                TestBtn.IsEnabled = false;
+                StopBtn.IsEnabled = true;
+                TestWDSwift.Client.TestNavChanged(DirTxt.Text);
+            }
+            else
+            {
+                MessageBox.Show("请输入程序地址", "警告", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void StopBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (StopBtn.IsEnabled)
+            {
+                TestWDSwift.Client.StopTest();
+                TestBtn.IsEnabled = true;
+                StopBtn.IsEnabled = false;
+            }
+        }
+        
+
+        /// <summary>
+        /// 注册全局热键
+        /// </summary>
+        private void RegisterHotKeys()
+        {
+            // need to setup the global hook. this can go in
+            // App.xaml.cs's constructor if you want
+            HotkeysManager.SetupSystemHook();
+
+            // You can create a globalhotkey object and pass it like so
+            HotkeysManager.AddHotkey(new GlobalHotkey(ModifierKeys.Control, Key.F4, () =>
+            {
+                StopBtn_Click(null, null);
+            }));
+
+            // or do it like this. both end up doing the same thing, but this is probably simpler.
+            HotkeysManager.AddHotkey(ModifierKeys.Shift, Key.F4, () =>
+            {
+                StopBtn_Click(null, null);
+            });
+
+        }
+    }
+}
---
+++ AutomationWPFTest/AutomationWPFTest/TestWDSwift.cs
@@ -0,0 +1,103 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.UIA3;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AutomationWPFTest
+{
+    internal class TestWDSwift
+    {
+        public static TestWDSwift Client { get; } = new TestWDSwift();
+
+        //public bool _isRunning { get; set; } = false;
+        //private UIA3Automation automation;
+        private readonly CancellationTokenSource _tokenSource = new CancellationTokenSource();
+        public void TestNavChanged(string path)
+        {
+            using var automation = new UIA3Automation();
+
+            Task.Run(async () =>
+            {
+                CheckStartProcess(path);
+
+
+                var app = FlaUI.Core.Application.Launch(path);
+
+                var loginWindow = app.GetMainWindow(automation);
+                Console.WriteLine(loginWindow.Title);
+
+                await Task.Delay(200);
+                var loginBtn = loginWindow.FindFirstDescendant(x => x.ByText("立 即 登 录")).AsButton();
+                loginBtn.Click(true);
+                await Task.Delay(2500);
+
+                var window = app.GetMainWindow(automation);
+                Console.WriteLine(loginWindow.Title);
+                var verticalToolBar = window.FindFirstDescendant(x => x.ByClassName("VerticalToolBar"));
+                var horizontalNavigation = window.FindFirstDescendant(x => x.ByClassName("HorizontalNavigation"));
+
+                var verticalListBox = verticalToolBar.FindFirstDescendant(x => x.ByAutomationId("lbMenuControl")).AsListBox();
+                var horizontalListBox = horizontalNavigation.FindFirstDescendant(x => x.ByAutomationId("lbMenuControl")).AsListBox();
+
+                try
+                {
+                    await Task.Run(async () =>
+                    {
+
+                        while (true)
+                        {
+                            foreach (var verticalItem in verticalListBox.Items)
+                            {
+                                verticalItem.Click(true);
+                                await Task.Delay(500);
+                                foreach (var horizontalItem in horizontalListBox.Items)
+                                {
+                                    horizontalItem.Click(true);
+                                    await Task.Delay(500);
+                                }
+                            }
+                        }
+                    }, _tokenSource.Token);
+                }
+                catch (Exception)
+                {
+                     // do nothing
+                }
+            });
+
+
+        }
+
+
+        public void StopTest()
+        {
+            //_isRunning = false;
+            _tokenSource.Cancel();
+        }
+        /// <summary>
+        /// 启动前检查，是否存在进程，存在则kill
+        /// </summary>
+        /// <param name="path"></param>
+        public void CheckStartProcess(string path)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(path);
+            Process[] pros = Process.GetProcessesByName(fileName);
+            for (int i = 0; i < pros.Length; i++)
+            {
+                if (pros[i].Id != Process.GetCurrentProcess().Id)
+                {
+                    pros[i].Kill();
+                }
+            }
+        }
+
+    }
+}
+
